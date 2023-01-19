15bd1542cb0a68b3da4b09f81247d706353b589a
Eric Avila <eric.avila@estudiante.uacm.edu.mx>
2023-01-19 14:45:55 -0600

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import { ThreeBody } from '@uiball/loaders'
+
+export const Loader = () => {
+  return (
+    <div className="container-loader">
+        <ThreeBody 
+ size={35}
+ speed={1.1} 
+ color="black" 
+/>
+    </div>
+  )
+}
